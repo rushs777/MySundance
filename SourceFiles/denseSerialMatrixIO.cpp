8e6b44eb9d060696a8418e5f298ba9ab14a2821a
rushs777 <simon.rush@ttu.edu>
2017-09-23 15:54:27 -0500

    Extended functionality be creating funtions to calculate uB, the vector for uB, and Wprime

--- SourceFiles/denseSerialMatrixIO.cpp
+++ SourceFiles/denseSerialMatrixIO.cpp
@@ -48,3 +48,53 @@ void readListExprMatrix(Expr &A, int N, string filename)
       A.append(row);
     }
 }
+
+
+
+
+
+Vector<double> meanVector(const LinearOperator<double> A)
+{
+  // Calculate the mean of the columns
+  Vector<double> avgVec = A.range().createMember();
+  Vector<double> ones = A.domain().createMember();
+  ones.setToConstant(1.0);
+  A.apply(ones, avgVec);
+  avgVec *= (1.0/ A.domain().dim() );
+  return avgVec;
+}
+
+
+Expr timeMeanFunctionGenerator(const LinearOperator<double> A, const DiscreteSpace ds)
+{
+  Expr avgFunc = new DiscreteFunction(ds, serialToEpetra(meanVector(A)));
+  return avgFunc;
+}
+
+
+
+
+
+LinearOperator<double> generateFluctuationMatrix(const LinearOperator<double> A)
+{
+  Vector<double> avgVec = meanVector(A);
+
+  Playa::VectorType<double> serialVecType = new Playa::SerialVectorType();
+  RCP<MatrixFactory<double> > mf = serialVecType.createMatrixFactory(A.domain(), A.range());
+  LinearOperator<double> rtnMtx = mf->createMatrix();
+  RCP<DenseSerialMatrix> rtnMtxPtr = DenseSerialMatrix::getConcretePtr(rtnMtx);
+
+  
+  RCP<const DenseSerialMatrix> APtr = DenseSerialMatrix::getConcretePtr(A);
+  double Aij;
+  for(int j = 0; j < A.domain().dim(); j++)
+    {
+      for(int i = 0; i<A.range().dim(); i++)
+	{
+	  Aij = APtr->getElement(i,j);
+	  rtnMtxPtr->setElement(i,j,Aij-avgVec[i]);
+	}  
+    }
+
+  return rtnMtx;
+}
--- SourceFiles/denseSerialMatrixIO.hpp
+++ SourceFiles/denseSerialMatrixIO.hpp
@@ -6,6 +6,9 @@
 #include "PlayaVectorImpl.hpp"
 #include "PlayaLinearOperatorImpl.hpp"
 #include "SundanceExpr.hpp"
+#include "PlayaSerialEpetraAdapter.hpp" // Allows me to convert between Epetra and Serial Vectors
+#include "SundanceDiscreteSpace.hpp"
+#include "SundanceDiscreteFunction.hpp"
 
 
 using std::vector;
@@ -14,6 +17,7 @@ using std::ifstream;
 using namespace Playa;
 using namespace PlayaExprTemplates;
 using Sundance::Expr;
+using Sundance::DiscreteSpace;
 
 /** 
  * This function writes the elements of the DenseSerialMatrix pointed to by A 
@@ -32,5 +36,22 @@ void readDenseSerialMatrix(RCP<DenseSerialMatrix>& A, string filename);
  */
 void readListExprMatrix(Expr &A, int N, string filename);
 
+/**
+ * This function returns a Vector<double> that is the mean of the columns of the matrix A
+ */
+Vector<double> meanVector(const LinearOperator<double> A);
+
+/**
+ * This function assumes that each column of the DenseSerialMatrix reprents velocity 
+ * and/or pressure data for a fluid in a spatial domain. It will return a spatial-only
+ * dependent function which is calculated as the time mean over all nodes of the mesh
+ */
+Expr timeMeanFunctionGenerator(const LinearOperator<double> A, const DiscreteSpace ds);
+
+/**
+ * Thus function returns a matrix for which the mean of the columns has been
+ * subtracted from each column for the supplied matrix
+ */
+LinearOperator<double> generateFluctuationMatrix(const LinearOperator<double> A);
 
 #endif
